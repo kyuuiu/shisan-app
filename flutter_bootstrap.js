9f8422471600f23c845e12e62b66b397aef6aea1
kyuuiu <kyuuiu@gmail.com>
2026-06-25 08:40:59 +0900

    Phase 1: screen skeleton (4 tabs, design applied)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3872140319" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "235588823" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
